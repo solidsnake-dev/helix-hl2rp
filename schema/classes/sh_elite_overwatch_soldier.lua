0bb1668bdaf7f6b6a54a4416138d8261cc92e57d
solidsnake-dev <thelegsoldierv2@gmail.com>
2026-07-24 04:48:38 +0200

    Update sh_elite_overwatch_soldier.lua

--- schema/classes/sh_elite_overwatch_soldier.lua
+++ schema/classes/sh_elite_overwatch_soldier.lua
@@ -2,12 +2,18 @@ CLASS.name = "Elite Overwatch Soldier"
 CLASS.faction = FACTION_OTA
 CLASS.isDefault = false
 
-function CLASS:OnSet(client)
-	local character = client:GetCharacter()
+function CLASS:CanSwitchTo(client)
+	local name = client:Name()
+	local bStatus = false
 
-	if (character) then
-		character:SetModel("models/combine_super_soldier.mdl")
+	for k, v in ipairs({ "EOWS", "EOS", "EOW", "ORD" }) do
+		if (Schema:IsCombineRank(name, v)) then
+			bStatus = true
+			break
+		end
 	end
+
+	return bStatus
 end
 
 CLASS_EOW = CLASS.index
