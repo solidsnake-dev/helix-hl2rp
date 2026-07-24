8971e442058d8c339136d126750481527f2ff71a
solidsnake-dev <thelegsoldierv2@gmail.com>
2026-07-24 04:44:05 +0200

    Update Pulse-Rifle attributes and class name

--- schema/items/weapons/sh_ar2.lua
+++ schema/items/weapons/sh_ar2.lua
@@ -1,13 +1,8 @@
 ITEM.name = "Pulse-Rifle"
-ITEM.description = "A dark energy powered assault rifle."
-ITEM.model = "models/weapons/w_irifle.mdl"
-ITEM.class = "weapon_ar2"
+ITEM.description = "A dark energy powered pulse rifle."
+ITEM.model = "models/weapons/w_irifle.mdl" -- placeholder, match to the TFA addon's world model
+ITEM.class = "tfa_hl2r_ar2" -- PLACEHOLDER - verify against the real TFA class name in-game
 ITEM.weaponCategory = "primary"
-ITEM.classes = {CLASS_EOW}
 ITEM.width = 4
 ITEM.height = 2
-ITEM.iconCam = {
-	ang	= Angle(-0.70499622821808, 268.25439453125, 0),
-	fov	= 12.085652091515,
-	pos	= Vector(0, 200, 0)
-}
+ITEM.category = "Weapons"
