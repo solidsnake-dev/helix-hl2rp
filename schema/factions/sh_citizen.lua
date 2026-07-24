9eb3a30e2cd4a302f74d19ff068897bbccfdb4d4
solidsnake-dev <thelegsoldierv2@gmail.com>
2026-07-24 17:51:29 +0200

    Add models for Citizen faction

--- schema/factions/sh_citizen.lua
+++ schema/factions/sh_citizen.lua
@@ -3,6 +3,16 @@ FACTION.name = "Citizen"
 FACTION.description = "A regular human citizen enslaved by the Universal Union."
 FACTION.color = Color(150, 125, 100, 255)
 FACTION.isDefault = true
+FACTION.models = {
+	"models/willardnetworks/citizens/female_01.mdl", "models/willardnetworks/citizens/female_02.mdl",
+	"models/willardnetworks/citizens/female_03.mdl", "models/willardnetworks/citizens/female_04.mdl",
+	"models/willardnetworks/citizens/female_06.mdl", "models/willardnetworks/citizens/female_07.mdl",
+	"models/willardnetworks/citizens/male01.mdl", "models/willardnetworks/citizens/male02.mdl",
+	"models/willardnetworks/citizens/male03.mdl", "models/willardnetworks/citizens/male04.mdl",
+	"models/willardnetworks/citizens/male05.mdl", "models/willardnetworks/citizens/male06.mdl",
+	"models/willardnetworks/citizens/male07.mdl", "models/willardnetworks/citizens/male08.mdl",
+	"models/willardnetworks/citizens/male09.mdl", "models/willardnetworks/citizens/male10.mdl",
+}
 
 function FACTION:OnCharacterCreated(client, character)
 	local id = Schema:ZeroNumber(math.random(1, 99999), 5)
